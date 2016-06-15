f0e20746499200119893124141e6d96fa529a474
Hunter McMillen <mcmillhj@gmail.com>
2016-06-14 22:37:57 -0400

    base commit, add travis config

--- lib/WUPHF.pm
+++ lib/WUPHF.pm
@@ -1,6 +1,24 @@
+# ABSTRACT: WUPHF, a blast messaging system 
 package WUPHF;
+use Moo;
 
 use strict;
 use warnings;
 
 1;
+
+=pod 
+
+=head1 NAME 
+
+WUPHF
+
+=head1 DESCRIPTION 
+
+WUPHF is a blast messaging system where users can link various services: Twitter, SMS, Facebook and receive notifications on all channels simultaneously
+
+=head4 SEE ALSO/INSPIRATION
+
+https://en.wikipedia.org/wiki/WUPHF.com
+
+=cut
